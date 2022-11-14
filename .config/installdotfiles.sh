9035b9c610d3417971316bcdeada53fa80d0203a
Dennis Pater <d.pater1986@gmail.com>
2022-11-14 14:01:20 +0100

    add installdotfiles script for a new maschine

---
+++ .config/installdotfiles.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+giturl='git@github.com:dpater1986/dotfiles.git'
+git clone --bare $giturl $HOME/.cfg
+function config {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+mkdir -p $HOME/.config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $HOME/.config-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
+
