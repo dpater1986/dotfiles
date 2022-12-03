40ab0c410f9cd224eb7623010f26106a670cf5e8
Dennis Pater <d.pater1986@gmail.com>
2022-12-03 18:13:12 +0100

    fix echo doesn't say what the script was doing

--- .config/installdotfiles.sh
+++ .config/installdotfiles.sh
@@ -6,12 +6,11 @@ git clone --bare $giturl $HOME/.cfg
 function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
-mkdir -p $HOME/.config-backup
 config checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
-    echo "Backing up pre-existing dot files.";
+    echo "Deleting pre-existing dot files.";
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $HOME/.config-backup/{}
 fi;
 config checkout
